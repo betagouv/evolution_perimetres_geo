290807315eb636036f30baad7d5c0e2723b15f20
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2021-09-28 10:13:35 +0200

    passage des géométries en Lambert 93 et ajout du millésime 2021

--- etl/src/downloads/ign.ts
+++ etl/src/downloads/ign.ts
@@ -10,8 +10,9 @@ export async function ignDownload():Promise<void>{
       fs.mkdirSync('./assets/ign', { recursive: true });
     }
     await Promise.all([
-      downloadFile(path,'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/ADMIN-EXPRESS-COG_2-0__SHP__FRA_WGS84G_2019-09-24.7z','admin-express-2019.7z'),
-      downloadFile(path,'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/ADMIN-EXPRESS-COG_2-1__SHP__FRA_WGS84G_2020-11-20.7z','admin-express-2020.7z')
+      downloadFile(path,'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/ADMIN-EXPRESS-COG_2-0__SHP__FRA_L93_2019-09-24.7z','admin-express-2019.7z'),
+      downloadFile(path,'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/ADMIN-EXPRESS-COG_2-1__SHP__FRA_L93_2020-11-20.7z','admin-express-2020.7z'),
+      downloadFile(path,'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/ADMIN-EXPRESS-COG_3-0__SHP__FRA_L93_2021-05-19.7z','admin-express-2021.7z'), 
     ])
   } 
   catch(err){
--- etl/src/transform/ign.ts
+++ etl/src/transform/ign.ts
@@ -4,18 +4,28 @@ import {convertGeoFile} from '../helpers/transformations'
 export async function ignTransform():Promise<void>{
   try{
     console.log('Start transform IGN files')
-    const path2019 =join(__dirname, '../../assets/ign/ADMIN-EXPRESS-COG_2-0__SHP__FRA_2019-09-24/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2019-09-24/ADE-COG_2-0_SHP_WGS84_FR/')
-    const path2020 =join(__dirname, '../../assets/ign/ADMIN-EXPRESS-COG_2-1__SHP__FRA_2020-11-20/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2020-11-20/ADE-COG_2-1_SHP_WGS84G_FRA/')
-    // simplification des géométries et transformation en geojson
+    const path2019 =join(__dirname, '../../assets/ign/ADMIN-EXPRESS-COG_2-0__SHP__FRA_2019-09-24/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2019-09-24/ADE-COG_2-0_SHP_LAMB93_FR/')
+    const path2020 =join(__dirname, '../../assets/ign/ADMIN-EXPRESS-COG_2-1__SHP__FRA_2020-11-20/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2020-11-20/ADE-COG_2-1_SHP_LAMB93_FR/')
+    const path2021 =join(__dirname, '../../assets/ign/ADMIN-EXPRESS-COG_3-0__SHP__FRA_2021-05-19/ADMIN-EXPRESS-COG/1_DONNEES_LIVRAISON_2021-05-19/ADECOG_3-0_SHP_LAMB93_FR/')
+    // transformation en geojson
     await Promise.all([
-      convertGeoFile(path2019,'COMMUNE_CARTO.shp',join(__dirname, '../../assets/ign/'),'communes_2019.geojson','geojson',0.000001,'-simplify dp interval=100 keep-shapes'),
+      convertGeoFile(path2019,'COMMUNE_CARTO.shp',join(__dirname, '../../assets/ign/'),'communes_2019.geojson','geojson',0.000001),
       convertGeoFile(path2019,'CHEF_LIEU_CARTO.shp',join(__dirname, '../../assets/ign/'),'chefs_lieux_2019.geojson','geojson',0.000001),
-      convertGeoFile(path2020,'COMMUNE.shp',join(__dirname, '../../assets/ign/'),'communes_2020.geojson','geojson',0.000001,' '+path2020+'ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force -simplify 60%'),
-      convertGeoFile(path2020,'CHEF_LIEU_CARTO.shp',join(__dirname, '../../assets/ign/'),'chefs_lieux_2020.geojson','geojson',0.000001,' '+path2020+'CHFLIEU_ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force')
+      convertGeoFile(path2020,'COMMUNE.shp',join(__dirname, '../../assets/ign/'),'communes_2020.geojson','geojson',0.000001,' '+path2020+'ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force'),
+      convertGeoFile(path2020,'CHEF_LIEU_CARTO.shp',join(__dirname, '../../assets/ign/'),'chefs_lieux_2020.geojson','geojson',0.000001,' '+path2020+'CHFLIEU_ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force'),
+      convertGeoFile(path2021,'COMMUNE.shp',join(__dirname, '../../assets/ign/'),'communes_2021.geojson','geojson',0.000001,' '+path2021+'ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force'),
+      convertGeoFile(path2021,'CHFLIEU_COMMUNE.shp',join(__dirname, '../../assets/ign/'),'chefs_lieux_2021.geojson','geojson',0.000001,' '+path2021+'CHFLIEU_ARRONDISSEMENT_MUNICIPAL.shp combine-files -merge-layers force')
     ])
-    // 2 simplifications supplémentaires pour la couche communes_2020
-    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_2020.geojson',join(__dirname, '../../assets/ign/'),'communes_2020.geojson','geojson',0.000001,'-simplify 50% keep-shapes',true)
-    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_2020.geojson',join(__dirname, '../../assets/ign/'),'communes_2020.geojson','geojson',0.000001,'-simplify 40% keep-shapes',true)
+    // Simplification des géométries pour les communes 2019
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_2019.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2019.geojson','geojson',0.000001,'-simplify dp interval=100 keep-shapes')
+    // Simplification des géométries pour les communes 2020 en 3 passages
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_2020.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2020.geojson','geojson',0.000001,'-simplify 60% keep-shapes',true)
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_light_2020.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2020.geojson','geojson',0.000001,'-simplify 50% keep-shapes',true)
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_light_2020.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2020.geojson','geojson',0.000001,'-simplify 40% keep-shapes',true)
+    // Simplification des géométries pour les communes 2021 en 3 passages
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_2021.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2021.geojson','geojson',0.000001,'-simplify 60% keep-shapes',true)
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_light_2021.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2021.geojson','geojson',0.000001,'-simplify 50% keep-shapes',true)
+    await convertGeoFile(join(__dirname, '../../assets/ign/'),'communes_light_2021.geojson',join(__dirname, '../../assets/ign/'),'communes_light_2021.geojson','geojson',0.000001,'-simplify 40% keep-shapes',true)
   } 
   catch(err){
     console.log(err)
