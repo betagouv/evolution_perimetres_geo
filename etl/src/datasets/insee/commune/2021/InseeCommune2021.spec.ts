449b87ef85ee4e14323602510427bc5b126b708e
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-01 10:15:35 +0200

    feat: add insee commune 2021 dataset

---
+++ etl/src/datasets/insee/commune/2021/InseeCommune2021.spec.ts
@@ -0,0 +1,66 @@
+import anyTest, { TestInterface } from 'ava';
+import { access } from 'fs/promises';
+import { Pool } from 'pg';
+import { AbstractDataset } from '../../../../common/AbstractDataset';
+import { createPool } from '../../../../helpers';
+import { InseeCommune2021 as Dataset } from './InseeCommune2021';
+
+interface TestContext {
+  connection: Pool;
+  dataset: AbstractDataset;
+}
+
+const test = anyTest as TestInterface<TestContext>;
+
+test.before(async (t) => {
+  t.context.connection = createPool();
+  t.context.dataset = new Dataset(t.context.connection);
+  await t.context.connection.query(`
+      DROP TABLE IF EXISTS ${t.context.dataset.table}
+    `);
+});
+
+test.after.always(async (t) => {
+  await t.context.connection.query(`
+      DROP TABLE IF EXISTS ${t.context.dataset.table}
+    `);
+});
+
+test.serial.skip('should validate', async (t) => {
+  await t.notThrowsAsync(() => t.context.dataset.validate(new Set()));
+});
+
+test.serial('should prepare', async (t) => {
+  await t.context.dataset.before();
+  const query = `SELECT * FROM ${t.context.dataset.table}`;
+  t.log(query);
+  await t.notThrowsAsync(() => t.context.connection.query(query));
+});
+
+test.serial('should download file', async (t) => {
+  await t.context.dataset.download();
+  t.true(t.context.dataset.filepaths.length >= 1);
+  for (const path of t.context.dataset.filepaths) {
+    await t.notThrowsAsync(() => access(path));
+  }
+});
+
+test.serial('should transform', async (t) => {
+  await t.notThrowsAsync(() => t.context.dataset.transform());
+});
+
+test.serial('should load', async (t) => {
+  await t.context.dataset.load();
+  const response = await t.context.connection.query(`
+      SELECT count(*) FROM ${t.context.dataset.table}
+    `);
+  t.is(response.rows[0].count, '37742');
+});
+
+test.serial.skip('should import', async (t) => {});
+
+test.serial.skip('should cleanup', async (t) => {
+  await t.context.dataset.after();
+  const query = `SELECT * FROM ${t.context.dataset.table}`;
+  await t.throwsAsync(() => t.context.connection.query(query));
+});
