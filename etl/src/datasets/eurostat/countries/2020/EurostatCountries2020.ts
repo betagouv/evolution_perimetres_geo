1d907972c01c5ed950ed83ff3fd474750eb7adb1
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2021-10-04 17:11:06 +0200

    fix stream Geojson

--- etl/src/datasets/eurostat/countries/2020/EurostatCountries2020.ts
+++ etl/src/datasets/eurostat/countries/2020/EurostatCountries2020.ts
@@ -51,6 +51,7 @@ export class EurostatCountries2020 extends AbstractDataset {
               `,
               values: [JSON.stringify(results.value).replace(/'/g, "''")],
             };
+            console.debug(query);
             await connection.query(query);
           }
         } while (!done);
