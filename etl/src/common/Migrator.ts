ee63754f10d9f484b8c5e5fb31e2b84e5af4e037
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-04 10:40:16 +0200

    feat: update migrator + test

--- etl/src/common/Migrator.ts
+++ etl/src/common/Migrator.ts
@@ -1,36 +1,35 @@
-import { getDatasetUuid } from '../helpers';
-import { StaticAbstractDataset } from '../interfaces';
 import { Pool } from 'pg';
 
 export class Migrator {
+  readonly table: string = 'dataset_migration';
+
   constructor(protected connection: Pool) {}
 
   async install(): Promise<void> {
     await this.connection.query(`
-            CREATE TABLE IF NOT EXISTS dataset_migration (
-                datetime timestamp NOT NULL DEFAULT NOW(),
-                dataset varchar(128) NOT NULL,
-                year smallint NOT NULL,
-                PRIMARY KEY (dataset, year)
-            )
-        `);
+      CREATE TABLE IF NOT EXISTS ${this.table} (
+          key varchar(128) PRIMARY KEY,
+          datetime timestamp NOT NULL DEFAULT NOW()
+      )
+    `);
   }
 
   async get(): Promise<Set<string>> {
     const result = await this.connection.query(`
-          SELECT dataset, year FROM dataset_migration
-        `);
-
-    return new Set(...result.rows.map((row) => getDatasetUuid(row.dataset, row.year)));
+      SELECT key FROM ${this.table} ORDER BY datetime ASC
+    `);
+    return new Set(result.rows.map((r) => r.key));
   }
 
-  async set(dataset: StaticAbstractDataset): Promise<void> {
-    await this.connection.query({
+  async set(key: string): Promise<void> {
+    const query = {
       text: `
-              INSERT INTO dataset_migration (dataset, year) VALUES (?, ?)
-              ON CONFLICT DO NOTHING
-            `,
-      values: [dataset.dataset, dataset.year],
-    });
+        INSERT INTO ${this.table} (key)
+          VALUES ($1)
+        ON CONFLICT DO NOTHING
+      `,
+      values: [key],
+    };
+    await this.connection.query(query);
   }
 }
