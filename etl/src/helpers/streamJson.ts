bc8ccdd1224072d8440b955052ae1c0b845d5c22
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-05 14:28:36 +0200

    fix: remove stream-chain

--- etl/src/helpers/streamJson.ts
+++ etl/src/helpers/streamJson.ts
@@ -1,12 +1,12 @@
 import { createReadStream } from 'fs';
 import { JsonOptions } from '../interfaces';
-import { chain } from 'stream-chain';
-import { parser } from 'stream-json';
-import { pick } from 'stream-json/filters/Pick';
+import { withParser } from 'stream-json/filters/Pick';
 import { streamArray } from 'stream-json/streamers/StreamArray';
 
 export async function* streamJson<T>(filepath: string, sheetOptions: JsonOptions, chunkSize = 100): AsyncIterable<T[]> {
-  const pipe = chain([createReadStream(filepath, { encoding: 'utf-8' }), parser(), pick(sheetOptions), streamArray()]);
+  const pipe = createReadStream(filepath, { encoding: 'utf-8' })
+    .pipe(withParser(sheetOptions))
+    .pipe(streamArray());
 
   let chunk: T[] = [];
   for await (const line of pipe) {
