716a7d57ada3ff80e59d6c9070d15054a85ffa6e
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-06 13:45:22 +0200

    fix: normalize varenv

--- etl/src/helpers/createPool.ts
+++ etl/src/helpers/createPool.ts
@@ -2,11 +2,11 @@ import { Pool, PoolConfig } from 'pg';
 
 export function createPool(config: PoolConfig = {}): Pool {
   return new Pool({
-    user: process.env.PG_USER || 'postgres',
-    password: process.env.PG_PASSWORD || 'postgres',
-    database: process.env.PG_DATABASE || 'local',
-    host: process.env.PG_HOST || '127.0.0.1',
-    port: process.env.PG_PORT ? parseInt(process.env.PG_PORT, 10) : 5432,
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database: process.env.POSTGRES_DB || 'local',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT, 10) : 5432,
     ...config,
   });
 }
