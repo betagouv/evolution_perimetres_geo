0e3521ecbee4523722a3af922e44074393d2a625
Nicolas Mérigot <nicolas@belenio.fr>
2021-10-06 15:49:09 +0200

    fix: logger

--- etl/src/helpers/getLogger.ts
+++ etl/src/helpers/getLogger.ts
@@ -1,6 +1,14 @@
-import createLogger from 'console-log-level';
-import { logger } from '../config';
+import { Console } from 'console';
+import { logger as loggerConfig } from '../config';
 
-export function getLogger() {
-  return createLogger(logger as any);
+export function getLogger(config = loggerConfig): Console {
+  const logger = new Console({ stdout: process.stdout, stderr: process.stderr });
+  const rewrite = ['log', 'debug', 'info', 'warn', 'error'];
+  const noop = () => {};
+  const index = rewrite.indexOf(config.level);
+  const splice = rewrite.splice(0, index);
+  return splice.reduce((obj, k) => {
+    obj[k] = noop;
+    return obj;
+  }, logger);
 }
