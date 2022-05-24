c023031c263e83b698427dea39809c8bcbef1be2
Julien Parmentier <julien.parmentier@pathtech.fr>
2022-05-24 17:23:10 +0200

    clean: Remove 7zip apt install, add 7zip node dependency and use it

--- etl/src/helpers/file/un7zipFile.ts
+++ etl/src/helpers/file/un7zipFile.ts
@@ -1,8 +1,9 @@
 import { extractFull } from 'node-7z';
+import { path7x } from '7zip-bin';
 
 export function un7zFile(filepath: string, extractPath: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    const stream = extractFull(filepath, extractPath);
+    const stream = extractFull(filepath, extractPath, { $bin: path7x });
     stream.on('end', resolve);
     stream.on('error', reject);
   });
