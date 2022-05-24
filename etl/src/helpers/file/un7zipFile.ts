0f30129e91f48688c475f7ae0e7a421834f4574c
Julien Parmentier <julien.parmentier@pathtech.fr>
2022-05-24 19:30:58 +0200

    fix: use 7za and add necessary chmod on bin : /

--- etl/src/helpers/file/un7zipFile.ts
+++ etl/src/helpers/file/un7zipFile.ts
@@ -1,9 +1,9 @@
 import { extractFull } from 'node-7z';
-import { path7x } from '7zip-bin';
+import { path7za } from '7zip-bin';
 
 export function un7zFile(filepath: string, extractPath: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    const stream = extractFull(filepath, extractPath, { $bin: path7x });
+    const stream = extractFull(filepath, extractPath, { $bin: path7za });
     stream.on('end', resolve);
     stream.on('error', reject);
   });
