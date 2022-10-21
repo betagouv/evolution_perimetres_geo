286d8c27d219f81e3f6c3c63b5179b95cbb4886e
Nicolas Mérigot <nicolas@belenio.fr>
2022-10-21 13:07:43 +0200

    wip: use docker compose and shell to upload mirror files

---
+++ docker/tools/upload-dataset-mirror.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# USAGE cat file | ./upload-dataset-mirror
+mc alias set minio $AWS_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+mkdir -p temp
+IN_S3=$(mc ls minio/$AWS_BUCKET --json | jq '.key')
+IFS=' : '
+while read hash_name url; do
+  FIND=$(grep "$hash_name" <<< $IN_S3)
+  echo "processing $url"
+  if [ -z $FIND ]
+    then
+        echo "not found"
+        curl -o ./temp/$hash_name "$url"
+        mc cp ./temp/$hash_name minio/$AWS_BUCKET
+    else
+        echo "found"
+    fi
+done < "${1:-/dev/stdin}"
