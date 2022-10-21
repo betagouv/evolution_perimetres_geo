24b0ec527f63793a730a18c40bc3f565e5d8413b
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2022-10-21 15:15:28 +0200

    fix secret declaration in docker-compose run command for mirroring files

--- etl/bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir temp
-IFS=' : '
-while read hash_name url; do
-    mc find ALIAS/PATH --name $hash_name
-    curl -o ./temp/$hash_name "$url"
-    echo "Download $hash_name : $url"
-done < test.txt
