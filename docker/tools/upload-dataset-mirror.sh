a1728ecc9db3dadb437090bd9bf6b5f339c7cca8
Nicolas Mérigot <nicolas@belenio.fr>
2022-10-25 11:16:46 +0200

    fix: use config instead of alias

--- docker/tools/upload-dataset-mirror.sh
+++ docker/tools/upload-dataset-mirror.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # USAGE cat file | ./upload-dataset-mirror
-mc alias set minio $AWS_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+mc config host add minio $AWS_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
 mkdir -p temp
 IN_S3=$(mc ls minio/$AWS_BUCKET --json | jq '.key')
 IFS=' : '
 while read hash_name url; do
   FIND=$(grep "$hash_name" <<< $IN_S3)
-  echo "processing $url"
+  echo "processing [$hash_name] $url"
   if [ -z $FIND ]
     then
         echo "not found"
