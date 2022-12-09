9db78a2bd4d3ac595be911d45abf723bc6bd9db4
Julien Parmentier <julien.parmentier@pathtech.fr>
2022-12-09 10:55:00 +0100

    fix: hardcode Mirror URL for ava local test

--- etl/ava.cjs
+++ etl/ava.cjs
@@ -3,4 +3,7 @@ module.exports = {
   require: ['ts-node/register'],
   timeout: '10m',
   files: ['src/**/*.spec.ts', '!src/**/*.integration.spec.ts'],
+  environmentVariables: {
+    "MIRROR_URL": "https://perimeters.s3.fr-par.scw.cloud"
+  }
 };
